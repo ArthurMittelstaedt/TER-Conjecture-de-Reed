090bf83130b57bdfc220854c7f61f79afc93ceec
DESKTOP-T1OJRDU\ASUS <boukhari_aicha@outlook.com>
2022-05-22 02:00:29 +0200

    rien changé

--- src/Graph_Gen/tests/Odometer_test.c
+++ src/Graph_Gen/tests/Odometer_test.c
@@ -1,7 +1,7 @@
 #include "../c/Odometer.c"
 
 int main(void) {
-    Odometer* o = newOdometer(9);
+    Odometer* o = newOdometer(8);
     printOdometer(o);
     while (inc(o)) {
         printOdometer(o);
